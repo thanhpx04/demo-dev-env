58db847f2da444585a7cc997d9642bcbc48a1558
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-07-27 14:52:42 +0700

    done show total count try show dialog

--- public/js/addon.js
+++ public/js/addon.js
@@ -2,26 +2,53 @@
 async function getGitHubInfo(issueKey) {
     const owner = 'thanhpx04';
     const repo = 'test-github-plugin';
-    const token = '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';
+    const token = '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';
     // const context = await AP.context.getContext();
     // var issueKey = context.jira.issue.key;
     console.log(issueKey);
-    const responseGet = await fetch(` https://api.github.com/repos/${owner}/${repo}/branches`, {
+    const responseBranches = await fetch(` https://api.github.com/repos/${owner}/${repo}/branches`, {
         headers: {
             "Accept": "application/json",
             "Authorization": `Bearer ${token}`}
     });
-    const dataGet = await responseGet.json();
-    let result = dataGet.filter(function (item) { return item.name.includes(issueKey); });
-    console.log(result);
-    console.log(result.length);
+    const dataBranches = await responseBranches.json();
+    let listFilteredBranches = dataBranches.filter(function (item) { return item.name.includes(issueKey); });
+    console.log(listFilteredBranches);
+    console.log(listFilteredBranches.length);
+    const responseCommits = await fetch(` https://api.github.com/repos/${owner}/${repo}/commits`, {
+        headers: {
+            "Accept": "application/json",
+            "Authorization": `Bearer ${token}`}
+    });
+    const dataCommits = await responseCommits.json();
+    let listFilteredCommits = dataCommits.filter(function (item) { return item.commit.message.includes(issueKey); });
+    console.log(listFilteredCommits);
+    console.log(listFilteredCommits.length);
+    displayData(listFilteredBranches.length, listFilteredCommits.length);
 }
 
-function getContext() {
-    AP.context.getContext().then(
-        response => {
-            console.log("getContext-LOG");
-            console.log(response);
-        }
-    );
+function displayData(noBranches, noCommits) {
+    const githubInfo = document.getElementById("githubInfo");
+
+    // clean data
+    $("#githubInfo").empty();
+
+    // populate data
+    githubInfo.innerHTML += 
+    `<div class="aui-message">
+    <p class="subtitle">No. branches: ${noBranches}</p>
+    <p class="subtitle">No. commits: ${noCommits}</p>
+    </div>`;
 }
+
+        // Shows the dialog when the "Show dialog" button is clicked
+        AJS.$("#dialog-show-button").on('click', function(e) {
+            e.preventDefault();
+            AJS.dialog2("#demo-dialog").show();
+        });
+
+        // Hides the dialog
+        AJS.$("#dialog-submit-button").on('click', function (e) {
+            e.preventDefault();
+            AJS.dialog2("#demo-dialog").hide();
+        });
