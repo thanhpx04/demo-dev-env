f1a9432b1228dbb127782953026428032bbd2915
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-07-27 12:48:25 +0700

    try out branches

--- public/js/addon.js
+++ public/js/addon.js
@@ -1,9 +1,20 @@
 /* App frontend script */
-function getGitHubInfo() {
-    // get issue key
+async function getGitHubInfo(issueKey) {
+    const owner = 'thanhpx04';
+    const repo = 'test-github-plugin';
+    const token = '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';
     // const context = await AP.context.getContext();
     // var issueKey = context.jira.issue.key;
     console.log(issueKey);
+    const responseGet = await fetch(` https://api.github.com/repos/${owner}/${repo}/branches`, {
+        headers: {
+            "Accept": "application/json",
+            "Authorization": `Bearer ${token}`}
+    });
+    const dataGet = await responseGet.json();
+    let result = dataGet.filter(function (item) { return item.name.includes(issueKey); });
+    console.log(result);
+    console.log(result.length);
 }
 
 function getContext() {
